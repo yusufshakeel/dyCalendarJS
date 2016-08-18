81ddcba93a3e86aabe86afb3dbfbbb72c12dd4fb
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2016-08-18 23:49:42 +0530

    initial commit of dycalendar code

---
+++ js/default.js
@@ -0,0 +1,7 @@
+dycalendar.draw({
+     target: '#dycalendar',
+     type: 'month',
+     month: 7,
+     year: 2016,
+     highlighttoday: true
+});
---
+++ js/dycalendar.js
@@ -0,0 +1,323 @@
+/*!
+ * dyCalendar is a JavaScript library for creating Calendar.
+ *
+ * Author: Yusuf Shakeel
+ * https://github.com/yusufshakeel
+ *
+ * GitHub Link: https://github.com/yusufshakeel/dyCalendarJS
+ *
+ * MIT license
+ * Copyright (c) 2016 Yusuf Shakeel
+ *
+ * Date: 2014-08-17 sunday
+ */
+
+(function(global) {
+
+    "use strict";
+
+    var
+    //this will be used by the user.
+        dycalendar = {},
+
+        //starting year
+        START_YEAR = 1900,
+
+        //end year
+        END_YEAR = 9999,
+
+        //name of the months
+        monthName = {
+            full: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            mmm: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+
+        //name of the days
+        dayName = {
+            full: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+            d: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+            dd: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+            ddd: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+        };
+
+    /**
+     * this function will draw Calendar Table
+     *
+     * @param object data   this contains the calendar data
+     * @param object option this is the settings object
+     * @return html
+     */
+    function drawCalendarMonthTable(data, option) {
+
+        var
+            table, tr, td,
+            div, container, elem,
+            r, c, count;
+
+        table = global.document.createElement("table");
+        tr = global.document.createElement("tr");
+
+        //create 1st row for the day letters
+        for (c = 0; c <= 6; c = c + 1) {
+            td = global.document.createElement("td");
+            td.innerHTML = "SMTWTFS"[c];
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+
+        //create 2nd row for dates
+        tr = global.document.createElement("tr");
+
+        //blank td
+        for (c = 0; c <= 6; c = c + 1) {
+            if (c === data.firstDayIndex) {
+                break;
+            }
+            td = global.document.createElement("td");
+            tr.appendChild(td);
+        }
+
+        //remaing td of dates for the 2nd row
+        count = 1;
+        while (c <= 6) {
+            td = global.document.createElement("td");
+            td.innerHTML = count;
+            if (data.today.date === count && data.today.monthIndex === data.monthIndex && option.highlighttoday === true) {
+                td.setAttribute("class", "dycalendar-today-date");
+            }
+            tr.appendChild(td);
+            count = count + 1;
+            c = c + 1;
+        }
+        table.appendChild(tr);
+
+        //create remaining rows
+        for (r = 3; r <= 7; r = r + 1) {
+            tr = global.document.createElement("tr");
+            for(c = 0; c <= 6; c = c + 1) {
+                if (count > data.totaldays) {
+                    r = 10; //to exit the outer loop
+                    break;
+                }
+                td = global.document.createElement('td');
+                td.innerHTML = count;
+                if (data.today.date === count && data.today.monthIndex === data.monthIndex && option.highlighttoday === true) {
+                    td.setAttribute("class", "dycalendar-today-date");
+                }
+                count = count + 1;
+                tr.appendChild(td);
+            }
+            table.appendChild(tr);
+        }
+
+        //calendar container
+        container = global.document.createElement("div");
+        container.setAttribute("class", "dycalendar-month-container");
+
+        //header div
+        div = global.document.createElement("div");
+        div.setAttribute("class", "dycalendar-header");
+
+        //month span
+        elem = global.document.createElement("span");
+        elem.setAttribute("class", "dycalendar-span-month-year");
+        if (option.monthformat === "mmm") {
+            elem.innerHTML = data.monthName + " " + data.year;
+        } else if (option.monthformat === "full") {
+            elem.innerHTML = data.monthNameFull + " " + data.year;
+        }
+
+        //add month span to header div
+        div.appendChild(elem);
+
+        //add header div to container
+        container.appendChild(div);
+
+        //body div
+        div = global.document.createElement("div");
+        div.setAttribute("class", "dycalendar-body");
+        div.appendChild(table);
+
+        //add body div to container div
+        container.appendChild(div);
+
+        //return container
+        return container;
+    }
+
+    /**
+     * this function will extend source object with defaults object.
+     *
+     * @param object source     this is the source object
+     * @param object defaults   this is the default object
+     * @return object
+     */
+    function extendSource(source, defaults) {
+        var property;
+        for (property in defaults) {
+            if (source.hasOwnProperty(property) === false) {
+                source[property] = defaults[property];
+            }
+        }
+        return source;
+    }
+
+    /**
+     * This function will return calendar detail.
+     *
+     * @param integer year		1900-9999 (optional) if not set will consider
+     *                          the current year.
+     * @param intger month		0-11 (optional) 0 = Jan, 1 = Feb, ... 11 = Dec,
+     *                          if not set will consider the current month.
+     * @return boolean|object	if error return false, else calendar detail
+     */
+    function getCalendar(year, month) {
+
+        var
+            dateObj = new Date(),
+            dateString,
+            result = {},
+            idx;
+
+        if (year < START_YEAR || year > END_YEAR) {
+            return false;
+        }
+        if (month > 11 || month < 0) {
+            return false;
+        }
+
+        //today
+        result.today = {};
+        dateString = dateObj.toString().split(" ");
+
+        idx = dayName.ddd.indexOf(dateString[0]);
+        result.today.dayIndex = idx;
+        result.today.dayName = dateString[0];
+        result.today.dayFullName = dayName.full[idx];
+
+        idx = monthName.mmm.indexOf(dateString[1]);
+        result.today.monthIndex = idx;
+        result.today.monthName = dateString[1];
+        result.today.monthNameFull = monthName.full[idx];
+
+        result.today.date = dateObj.getDate();
+
+        result.today.year = dateString[3];
+
+        //get month-year first day
+        dateObj.setFullYear(year);
+        dateObj.setMonth(month);
+        dateObj.setDate(1);
+        dateString = dateObj.toString().split(" ");
+
+        idx = dayName.ddd.indexOf(dateString[0]);
+        result.firstDayIndex = idx;
+        result.firstDayName = dateString[0];
+        result.firstDayFullName = dayName.full[idx];
+
+        idx = monthName.mmm.indexOf(dateString[1]);
+        result.monthIndex = idx;
+        result.monthName = dateString[1];
+        result.monthNameFull = monthName.full[idx];
+
+        result.year = dateString[3];
+
+        dateObj.setFullYear(year);
+        dateObj.setMonth(month + 1);
+        dateObj.setDate(0);
+        result.totaldays = dateObj.getDate();
+
+        return result;
+
+    }
+
+    //------------------------------ dycalendar.draw() ----------------------
+
+    /**
+     * this function will draw the calendar based on user preferences.
+     *
+     * option = {
+     *  target : "#id"          //(mandatory) for id use #id
+     *  type : "calendar-type"  //(optional) values: "today|month" (default "today")
+     *  month : "integer"       //(optional) value 0-11, where 0 = January, ... 11 = December (default current month)
+     *  year : "integer"        //(optional) example 1990. (default current year)
+     *  monthformat : "full"    //(optional) values: "mmm|full" (default "full")
+     *  highlighttoday : boolean    //(optional) (default false) if true will highlight today's date
+     * }
+     *
+     * @param object option     user preferences
+     * @return boolean          true if success, false otherwise
+     */
+    dycalendar.draw = function (option) {
+
+        //check if option is passed or not
+        if(typeof option === "undefined") {
+            return false;
+        }
+
+        var
+            self = this,    //pointing at dycalendar object
+
+            dateObj = new Date(),
+
+            //default settings
+            defaults = {
+                type : "today",
+                month : dateObj.getMonth(),
+                year : dateObj.getFullYear(),
+                monthformat : "full",
+                highlighttoday : false
+            },
+
+            //variables for creating calendar
+            calendar,
+            calendarHTML,
+            targetedElementBy = "id",
+            targetedClass,
+
+            //other variables
+            i, len;
+
+        //find target element by
+        if (option.target[0] === "#") {
+            targetedElementBy = "id";
+        }
+        option.target = option.target.substring(1);
+
+        //extend user options with predefined options
+        option = extendSource(option, defaults);
+
+        global.console.log(option);
+
+        //get calendar HTML
+        switch (option.type) {
+        case "today":
+            calendarHTML = "<p>Today</p>";
+            break;
+
+        case "month":
+            //get calendar detail
+            calendar = getCalendar(option.year, option.month);
+            calendarHTML = drawCalendarMonthTable(calendar, option);
+            break;
+
+        default:
+            return false;
+        }
+
+        //draw calendar
+        if (targetedElementBy === "id") {
+
+            global.document.getElementById(option.target).innerHTML = null;
+            global.document.getElementById(option.target).appendChild(calendarHTML);
+
+        }
+
+    };
+
+    //------------------------------ dycalendar.draw() ends here ------------
+
+    //attach to global window object
+    global.dycalendar = dycalendar;
+
+}(typeof window !== "undefined" ? window : this));
