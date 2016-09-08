6cad0cc89d2bb67bee9f6cb61995e5250a7d3b96
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2016-09-08 21:50:45 +0530

    code update

--- js/default.js
+++ js/default.js
@@ -14,30 +14,64 @@
 
 //today calendar
 dycalendar.draw({
-     target: '#dycalendar-today',
-     dayformat: "full",
-     monthformat: "full"
-});
-
-//day calendar
-dycalendar.draw({
-     target: '#dycalendar-day',
-     type: 'day',
-     dayformat: "full",
-     monthformat: "full",
-     date : 21,
-     month : 9,
-     year : 1990
+     target: '#dycalendar-today'
 });
 
 //month calendar
 dycalendar.draw({
      target: '#dycalendar-month',
-     type: 'month',
+     type: 'month'
+});
+
+//day calendar
+dycalendar.draw({
+     target: '#dycalendar-day',
+     type : "day",
      date : 21,
      month : 9,
-     year : 2016,
-     highlighttoday: true,
-     highlighttargetdate: true,
+     year : 1990,
+     highlighttargetdate : true
+});
+
+//today calendar - with skin
+dycalendar.draw({
+     target: '#dycalendar-today-with-skin',
+     type: 'day',
+     dayformat: "ddd",
+     monthformat: "mmm"
+});
+
+//today calendar - with skin and shadow
+dycalendar.draw({
+     target: '#dycalendar-today-with-skin-shadow',
+     type: 'day',
+     dayformat: "full",
      monthformat: "full"
+});
+
+//month calendar - default current month - with skin
+dycalendar.draw({
+     target: '#dycalendar-month-with-skin',
+     type: 'month',
+     highlighttoday: true,
+     monthformat: "mmm"
+});
+
+//month calendar - with skin and shadow
+dycalendar.draw({
+     target: '#dycalendar-month-with-skin-shadow',
+     type: 'month',
+     month: 9,
+     year: 1990,
+     date: 21,
+     monthformat: "full",
+     highlighttargetdate : true
+});
+
+//month calendar - with skin and shadow - target class
+dycalendar.draw({
+     target: '.dycalendar-month',
+     type: 'month',
+     monthformat: "full",
+     highlighttargetdate : true
 });
