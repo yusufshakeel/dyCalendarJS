9039f7ddd4b3b2723ba9cd22e5790700f585b604
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2016-09-13 23:29:04 +0530

    update

--- js/dycalendar.js
+++ js/dycalendar.js
@@ -458,7 +458,7 @@
      * this function will draw the calendar based on user preferences.
      *
      * option = {
-     *  target : "#id|.class"   //(mandatory) for id use #id | for class user .class
+     *  target : "#id|.class"   //(mandatory) for id use #id | for class use .class
      *  type : "calendar-type"  //(optional) values: "day|month" (default "day")
      *  month : "integer"       //(optional) value 0-11, where 0 = January, ... 11 = December (default current month)
      *  year : "integer"        //(optional) example 1990. (default current year)
